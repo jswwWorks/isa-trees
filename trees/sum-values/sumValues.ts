926722acba9e50879ae7f5cede5e6b2113e1d4fa
Matt Smida <mcs@m4t7.com>
2024-02-28 18:38:53 -0500

    Finished sumValues (with comments)

--- trees/sum-values/sumValues.ts
+++ trees/sum-values/sumValues.ts
@@ -4,18 +4,24 @@ import { TreeNode, TreeNodeNum } from "../common/tree";
 
 /** sumValues(): add up all values of node and its descendants.
  * Returns sum as an integer. */
-function sumValues(node: TreeNodeNum): number {
+function sumValues(node: TreeNodeNum): number {    // always type the return!
+  // why? if you change what it returns, it won't silently infer the wrong return.
 
   let sum: number = 0;
 
   // let toVisit = new Stack<TreeNode>([this])
 
   // say this line is getting what we want: a treenode for the spot we're at
-  let toVisit: [] = new TreeNode<TreeNodeNum[]>([node]);
+  // let toVisit: [] = new TreeNode<TreeNodeNum[]>([node]);
+  // let toVisit = new TreeNode<TreeNodeNum[]>([node]);
+  // let toVisit: TreeNodeNum[] = [];
+
+  // let toVisit: TreeNode<number>[] = [node];
+
+  let toVisit = [node];
 
   while (toVisit.length > 0) {
-    const current: TreeNodeNum = toVisit.pop(); //
-
+    const current = toVisit.pop()!; // ! if you know it won't be empty
     sum += current.val;
 
     for (const child of current.children) {
