310ce706694a62abfd12a2fb9ce448ab13c76cd0
jswwWorks <jsww.works@gmail.com>
2024-02-28 15:12:01 -0500

    Drafted sumValues pseudocode

--- trees/sum-values/sumValues.ts
+++ trees/sum-values/sumValues.ts
@@ -1,9 +1,29 @@
-import { TreeNodeNum } from "../common/tree";
+"use strict";
+
+import { TreeNode, TreeNodeNum } from "../common/tree";
 
 /** sumValues(): add up all values of node and its descendants.
  * Returns sum as an integer. */
 function sumValues(node: TreeNodeNum): number {
-  return 42;
+
+  let sum: number = 0;
+
+  // let toVisit = new Stack<TreeNode>([this])
+
+  // say this line is getting what we want: a treenode for the spot we're at
+  let toVisit: [] = new TreeNode<TreeNodeNum[]>([node]);
+
+  while (toVisit.length > 0) {
+    const current: TreeNodeNum = toVisit.pop(); //
+
+    sum += current.val;
+
+    for (const child of current.children) {
+      toVisit.push(child);
+    }
+  }
+
+  return sum;
 }
 
 export { sumValues };
