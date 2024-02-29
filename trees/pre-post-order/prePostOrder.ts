739b4f00e5f13cdaa60635f487cac4032e73cfa5
jswwWorks <jsww.works@gmail.com>
2024-02-29 14:58:33 -0500

    Progress on recursive solution to postOrder function

--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -10,8 +10,8 @@ function preOrder(node: BNodeNum | null): number[] {
   while (toVisit.length > 0) {
     // if (current. is null, you can return here)
     const current = toVisit.shift()!;
-    console.log("current: ", current);
-    console.log("toVisit: ", toVisit);
+    // console.log("current: ", current);
+    // console.log("toVisit: ", toVisit);
     if (current) {
       orderedVals.push(current.val);
       for (const child of current.children) {
@@ -30,7 +30,44 @@ function preOrder(node: BNodeNum | null): number[] {
  * Returns an array of visited nodes. */
 
 function postOrder(node: BNodeNum | null): number[] {
-  return [42];
+
+  if (node === null) return []; // if initial node is null
+  if (node.children.length === 0) return node.val; // if we're at a leaf node
+
+  // but if it does have children
+  for (const child of node.children) {
+    console.log("in for loop");
+
+    console.log([...postOrder(child), node.val]);
+
+    return [...postOrder(child), node.val];
+  }
+
+  return [];
 }
 
 export { preOrder, postOrder };
+
+
+// function postOrder(node: BNodeNum | null): number[] {
+
+//   let orderedVals: number[] = [];
+//   let toVisit = [node];
+
+//   while (toVisit.length > 0) {
+//     // if (current. is null, you can return here)
+//     const current = toVisit.shift()!;
+
+//     if (current) {
+//       for (const child of current.children) {
+//         toVisit.push(child);
+//       }
+//       orderedVals.push(current.val);
+//     }
+
+//     if (toVisit.length === 0) return orderedVals;
+//   }
+
+//   return orderedVals;
+
+// }
