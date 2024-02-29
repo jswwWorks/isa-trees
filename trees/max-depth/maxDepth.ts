67f229ff4454c72ca3564cb3bdda4dd11d0052fa
jswwWorks <jsww.works@gmail.com>
2024-02-28 20:25:14 -0500

    Drafted partial version of maxDepth using recursion this time.

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -7,35 +7,57 @@ import { TreeNodeNum } from "../common/tree";
 
 function maxDepth(node: TreeNodeNum | null): number {
 
-  let maxDepth: number = 0;
-  let currentDepth: number = 0;
-  let prevMaxDepth:
+  // base case:
+  // what the job is
+  // when do we do the recursive call:
 
-  let toVisit = [node];
+  // base case
 
-  while (toVisit.length > 0) {
-
-    const current: TreeNodeNum = toVisit.pop()!;
-    currentDepth++;
-    if (currentDepth > maxDepth) maxDepth = currentDepth;
-
-    console.log("current.children: ", current.children);
-    console.log("current.children.length: ", current.children.length);
-
-    if (!current.children) {
-      // currentDepth--;
-      currentDepth = maxDepth;
-      console.log("value: ", current.val);
-      console.log("depth : ", currentDepth);
-    }
-
-    for (const child of current.children) {
-      toVisit.push(child);
-    }
+  if (node === null) return 0;
+  if (node.children.length === 0) return 1;
 
 
+  for (const child of node.children) {
+    console.log('maxDepth') // need to get max
+    return maxDepth(child!) + 1;
   }
-  return maxDepth;
+
+
 }
 
 export { maxDepth };
+
+
+// function maxDepth(node: TreeNodeNum | null): number {
+
+//   let maxDepth: number = 0;
+//   let currentDepth: number = 0;
+//   let prevMaxDepth: number = 0;
+
+//   let toVisit = [node];
+
+//   while (toVisit.length > 0) {
+
+//     const current: TreeNodeNum = toVisit.pop()!;
+//     currentDepth++;
+//     if (currentDepth > maxDepth) maxDepth = currentDepth;
+
+//     console.log("current.children: ", current.children);
+//     console.log("current.children.length: ", current.children.length);
+
+//     if (!current.children) {
+//       // currentDepth--;
+//       currentDepth = maxDepth;
+//       console.log("value: ", current.val);
+//       console.log("depth : ", currentDepth);
+//     }
+
+//     for (const child of current.children) {
+//       toVisit.push(child);
+//     }
+
+
+
+//   }
+//   return maxDepth;
+// }
