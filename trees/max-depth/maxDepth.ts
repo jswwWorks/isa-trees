1728a4f24c1b71d8ce384019671e548cf202dbce
Matt Smida <mcs@m4t7.com>
2024-02-28 20:40:05 -0500

    Finished maxDepth().

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -7,57 +7,10 @@ import { TreeNodeNum } from "../common/tree";
 
 function maxDepth(node: TreeNodeNum | null): number {
 
-  // base case:
-  // what the job is
-  // when do we do the recursive call:
-
-  // base case
-
   if (node === null) return 0;
   if (node.children.length === 0) return 1;
 
-
-  for (const child of node.children) {
-    console.log('maxDepth') // need to get max
-    return maxDepth(child!) + 1;
-  }
-
-
+  return Math.max(...node.children.map(c => maxDepth(c) + 1));
 }
 
 export { maxDepth };
-
-
-// function maxDepth(node: TreeNodeNum | null): number {
-
-//   let maxDepth: number = 0;
-//   let currentDepth: number = 0;
-//   let prevMaxDepth: number = 0;
-
-//   let toVisit = [node];
-
-//   while (toVisit.length > 0) {
-
-//     const current: TreeNodeNum = toVisit.pop()!;
-//     currentDepth++;
-//     if (currentDepth > maxDepth) maxDepth = currentDepth;
-
-//     console.log("current.children: ", current.children);
-//     console.log("current.children.length: ", current.children.length);
-
-//     if (!current.children) {
-//       // currentDepth--;
-//       currentDepth = maxDepth;
-//       console.log("value: ", current.val);
-//       console.log("depth : ", currentDepth);
-//     }
-
-//     for (const child of current.children) {
-//       toVisit.push(child);
-//     }
-
-
-
-//   }
-//   return maxDepth;
-// }
